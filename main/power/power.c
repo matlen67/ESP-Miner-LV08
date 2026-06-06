a70607ca0c86773aa52c62f16bb3fa7a1fb380b7
matlen67 <45318154+matlen67@users.noreply.github.com>
2026-06-06 08:52:41 +0200

    Adapted for LV08

--- main/power/power.c
+++ main/power/power.c
@@ -10,8 +10,8 @@ void Power_get_output(GlobalState * GLOBAL_STATE, float * power_out, float * cur
     float pow_val = 0.0f;
 
     if (GLOBAL_STATE->DEVICE_CONFIG.TPS546) {
-        float iout = TPS546_get_iout();
-        float vout = TPS546_get_vout();
+        float iout = TPS546_get_iout(0);
+        float vout = TPS546_get_vout(0);
         cur_val = iout * 1000.0f;
         // The power reading from the TPS546 is only it's output power. So the rest of the Bitaxe power is not accounted for.
         pow_val   = vout * iout;
@@ -22,6 +22,20 @@ void Power_get_output(GlobalState * GLOBAL_STATE, float * power_out, float * cur
         pow_val = INA260_read_power() / 1000.0f;
     }
 
+    if (GLOBAL_STATE->DEVICE_CONFIG.TPS546_LV08) {
+        float iout = TPS546_get_iout(0);
+
+        for (int addr = 1; addr < 3; addr++) {
+            float i = TPS546_get_iout(addr);
+            if (i > iout)
+                iout = i;
+        }
+        cur_val = iout * 1000.0f;
+
+        pow_val   = vout * iout;
+        pow_val  += GLOBAL_STATE->DEVICE_CONFIG.family.power_offset;
+    }
+    
     *current_out = cur_val;
     *power_out   = pow_val;
 }
@@ -29,10 +43,22 @@ void Power_get_output(GlobalState * GLOBAL_STATE, float * power_out, float * cur
 float Power_get_input_voltage(GlobalState * GLOBAL_STATE)
 {
     if (GLOBAL_STATE->DEVICE_CONFIG.TPS546) {
-        return TPS546_get_vin() * 1000.0;
+        return TPS546_get_vin(0) * 1000.0;
     }
     if (GLOBAL_STATE->DEVICE_CONFIG.INA260) {
         return INA260_read_voltage();
+    }
+    
+    if (GLOBAL_STATE->DEVICE_CONFIG.TPS546_LV08) {
+        float vin = TPS546_get_vin(0);
+
+        for (int addr = 1; addr < 3; addr++) {
+            float v = TPS546_get_vin(addr);
+            if (v > vin)
+            vin = v;
+        }
+
+        return vin * 1000.0f;
     }
     
     return 0.0;
@@ -41,7 +67,19 @@ float Power_get_input_voltage(GlobalState * GLOBAL_STATE)
 float Power_get_vreg_temp(GlobalState * GLOBAL_STATE)
 {
     if (GLOBAL_STATE->DEVICE_CONFIG.TPS546) {
-        return TPS546_get_temperature();
+        return TPS546_get_temperature(0);
+    }
+
+     if (GLOBAL_STATE->DEVICE_CONFIG.TPS546_LV08) {
+        float temp = TPS546_get_temperature(0);
+
+        for (int addr = 1; addr < 3; addr++) {
+            float t = TPS546_get_temperature(addr);
+            if (t > temp)
+                temp = t;
+        }
+
+        return temp;
     }
 
     return 0.0;
