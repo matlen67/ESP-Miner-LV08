27bd50ef09f6a02c1dc6ea06481f7cc53f86d4a8
matlen67 <45318154+matlen67@users.noreply.github.com>
2026-07-17 16:08:08 +0200

    adapted for LV08

--- main/power/power.c
+++ main/power/power.c
@@ -23,18 +23,16 @@ void Power_get_output(GlobalState * GLOBAL_STATE, float * power_out, float * cur
     }
 
     if (GLOBAL_STATE->DEVICE_CONFIG.TPS546_LV08) {
-        float iout = TPS546_get_iout(0);
-        float vout = TPS546_get_vout(0);
+        cur_val = 0.0f;
+        pow_val = 0.0f;
 
-        for (int addr = 1; addr < 3; addr++) {
-            float i = TPS546_get_iout(addr);
-            if (i > iout)
-                iout = i;
+        for (int addr = 0; addr < 3; addr++) {
+            float iout = TPS546_get_iout(addr);
+            float vout = TPS546_get_vout(addr);
+            cur_val += iout * 1000.0f;
+            pow_val += vout * iout;
         }
         
-        cur_val = iout * 1000.0f;
-
-        pow_val   = vout * iout;
         pow_val  += GLOBAL_STATE->DEVICE_CONFIG.family.power_offset;
     }
     
