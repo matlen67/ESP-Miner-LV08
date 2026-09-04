1979f314633d1d8d58ef58a6bb74d964a9cc4af9
matlen67 <45318154+matlen67@users.noreply.github.com>
2026-09-04 14:46:10 +0200

    patch for LV08

--- main/self_test/self_test.h
+++ main/self_test/self_test.h
@@ -6,7 +6,7 @@
 typedef struct GlobalState GlobalState;
 
 esp_err_t self_test_init(GlobalState * GLOBAL_STATE);
-void self_test_task(void * pvParameters);
+void self_test_task(void * pvParameters, int8_t i2c_addr);
 void self_test_show_message(GlobalState * GLOBAL_STATE, const char * msg);
 void self_test_reset(void);
 void self_test_record_nonce(GlobalState * GLOBAL_STATE, double nonce_diff);
